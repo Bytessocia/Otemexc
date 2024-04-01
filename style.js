f108e2f72542ff4d14473a7d4f55fa7d5a065bea
Nearby Connect - Local dating <142696184+Bytessocia@users.noreply.github.com>
2024-04-01 01:05:27 +0100

    Update style.js

--- style.js
+++ style.js
@@ -40,10 +40,14 @@ swap.addEventListener('click', function() {
 
 calculate();
 
-
-
-
-
-
-
-
+<!--Start of Tawk.to Script-->
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66040cd8a0c6737bd125672c/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+<!--End of Tawk.to Script-->
